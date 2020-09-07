7523a2f13d3983c70023f92e24e4ede77d896b2e
Dmytro Prysiazhnyi <dimpris@gmail.com>
2020-09-08 01:38:30 +0300

    Configured Gulp and dev server

---
+++ gulpfile.js
@@ -0,0 +1,64 @@
+
+// FROM off doc
+
+var gulp = require("gulp");
+var server = require('gulp-server-livereload');
+var browserify = require("browserify");
+var source = require("vinyl-source-stream");
+var watchify = require("watchify");
+var tsify = require("tsify");
+var fancy_log = require("fancy-log");
+var paths = {
+  pages: ["src/*.html"],
+};
+
+var watchedBrowserify = watchify(
+  browserify({
+    basedir: ".",
+    debug: true,
+    entries: ["src/main.ts"],
+    cache: {},
+    packageCache: {},
+  }).plugin(tsify)
+);
+
+gulp.task("copy-html", function () {
+  return gulp.src(paths.pages).pipe(gulp.dest("dist"));
+});
+
+function bundle() {
+  return watchedBrowserify
+    .bundle()
+    .on("error", fancy_log)
+    .pipe(source("bundle.js"))
+    .pipe(gulp.dest("dist"));
+}
+
+gulp.task("watch", gulp.series(gulp.parallel("copy-html"), bundle));
+watchedBrowserify.on("update", bundle);
+watchedBrowserify.on("log", fancy_log);
+
+gulp.task('webserver', function() {
+    gulp.src('dist')
+      .pipe(server({
+        fallback: 'index.html',
+        livereload: {
+            enable: true,
+        },
+        open: true
+      }));
+});
+
+// gulp.task('webserver', ['watch'], function () {
+//     gulp.src('dist')
+//       .pipe(server({
+//         fallback: 'index.html',
+//         livereload: {
+//           enable: true,
+//         },
+//         directoryListing: true,
+//         open: true
+//       }));
+//   });
+
+gulp.task("default", gulp.series("watch", "webserver"));
