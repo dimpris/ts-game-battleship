ed7effa988c5d5fbc3d44c7eda4ae62bdb61ef74
Dmytro Prysiazhnyi <dimpris@gmail.com>
2020-10-13 13:46:23 +0300

    Render field with cells and numeration at the left vertical column

--- src/core/models/field.model.ts
+++ src/core/models/field.model.ts
@@ -2,12 +2,49 @@ import { Cell } from "./cell.model";
 
 export class Field {
     public cells: Cell[] = [];
+    private canvas: HTMLCanvasElement;
+    private context: CanvasRenderingContext2D;
+
+    public constructor(canvas: HTMLCanvasElement) {
+        this.canvas = canvas;
+        this.context = this.canvas.getContext("2d");
+    }
 
     public AddCell(cell: Cell) {
         this.cells.push(cell);
     }
 
     public render () {
-        // 
+        this.context.fillStyle = "#FEFEFE";
+        this.context.fillRect(5, 5, 385, 385);
+        this.context.fillRect(395, 5, 385, 385);
+
+        this.context.fillStyle = "#000000";
+        this.context.strokeStyle="rgba(0,0,0,1)";
+        
+
+        for (let i = 0; i < 11; i++) {
+            for (let j = 0; j < 11; j++) {
+                let x = 5 + ( 35 * i);
+                let y = 5 + ( 35 * j);
+                this.context.strokeRect(x, y, 35, 35);
+            }
+        }
+
+        for (let i = 0; i < 11; i++) {
+            for (let j = 0; j < 11; j++) {
+                let x = 395 + ( 35 * i);
+                let y = 5 + ( 35 * j);
+                this.context.strokeRect(x, y, 35, 35);
+            }
+        }
+
+        this.context.font = "17px Arial";
+        for (let i = 0; i < 10; i++) {
+            let num = i + 1;
+            let y = 35 + (28 * 1.25 * num);
+            this.context.fillText(num.toString(), 13, y);
+            this.context.fillText(num.toString(), 403, y);
+        }
     }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,6 @@ import { CoordinateX, CoordinateY } from "./core/enums";
 import { Field } from "./core/models/field.model";
 
 
-const canvas = document.querySelector("#battleship-canvas");
-const context = canvas.getContext("2d");
-context.moveTo(0, 0);
-context.lineTo(400, 400);
-context.stroke();
-const field = new Field();
-
-const c1 = new Coordinate(CoordinateX.C, CoordinateY.Nine);
-const c2 = new Coordinate(CoordinateX.E, CoordinateY.Three);
-const c3 = new Coordinate(CoordinateX.G, CoordinateY.Seven);
-
-field.AddCell(new Cell(c1));
-field.AddCell(new Cell(c2));
-field.AddCell(new Cell(c3));
-
-console.log(field);
+const canvas = document.querySelector("#battleship-canvas") as HTMLCanvasElement;
+const field = new Field(canvas);
+field.render();
